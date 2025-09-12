74957a11a884197042fc3cf9891048a3472799f6
hedjouj <messaoui.elhadj@hotmail.fr>
2025-09-12 11:16:28 +0200

    feat: 4. Writing a class and 3. Printing teachers

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -19,3 +19,32 @@ const director1: Directors = {
   numberOfReports: 17,
 };
 console.log(director1);
+
+const printTeacher: prinTeacherFunction = (firstName, lastName) => {
+    return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+interface prinTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  constructor(public firstName: string, public lastName: string) {}
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
